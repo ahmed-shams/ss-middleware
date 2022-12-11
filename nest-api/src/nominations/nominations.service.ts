33d3e506678f38e08abfde005144215f97377c93
Ahmed Shams <zangee3@gmail.com>
2022-12-10 19:58:01 -0600

    fix typerror

--- nest-api/src/nominations/nominations.service.ts
+++ nest-api/src/nominations/nominations.service.ts
@@ -78,7 +78,7 @@ export class NominationsService {
       state
     });
 
-    this.logService.log(id, "Getting Report from Second Street");
+    this.logService.log(id, "Getting Report from Second Street, (will take approx. 2 minutes)");
     const reportJson: any = await this.reportsService.getWinnersReport(organizationId, promotionId, organizationPromotionId);
     this.logService.log(id, `second result ${reportJson}`);
     await this.parseWinnerReport(reportJson.reports[0].file_url, id);
